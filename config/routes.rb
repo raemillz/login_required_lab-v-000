7b863f5ba5758292de40ec9fe0905c90f1b0fa2d
Rachel Miller <raeanne1991@gmail.com>
2018-04-12 21:41:55 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'application#welcome'
-    get '/login' => 'sessions#new'
-    post '/login' => 'sessions#create'
-    post '/login' => 'sessions#destroy'
+    resources :sessions
 
 end
