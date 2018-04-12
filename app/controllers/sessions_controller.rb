e058538bcf2540ec0d6c8d854f0a70938bbed5b1
Rachel Miller <raeanne1991@gmail.com>
2018-04-12 22:43:16 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      flash[:alert] = "You must input a name to login."
+      redirect_to '/new'
+    else
+      session[:name] = params[:name]
+      redirect_to 'sessions/show'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
+
+  def show
+    
+  end
+
+end
