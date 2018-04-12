9460c84e2a39efae13a9456c692892b5e50c7372
Rachel Miller <raeanne1991@gmail.com>
2018-04-12 22:15:50 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'new', to: 'sessions#new'
   post 'logout', to: 'sessions#destroy'
   post 'create', to: 'sessions#create'
-  get 'show', to: 'sessions#show'
+  post 'show', to: 'sessions#show'
 
   get '/secret', to: 'secrets#show'
 end
