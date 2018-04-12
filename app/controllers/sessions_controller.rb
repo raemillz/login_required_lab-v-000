9aa5a863e149c1468b8f8e41490e7fc7889940db
Rachel Miller <raeanne1991@gmail.com>
2018-04-12 21:53:24 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:name] = params[:name]
       redirect_to 'sessions/show'
     else
-      alert: "You must input a name to login."
+      flash[:alert] = "You must input a name to login."
       redirect_to '/'
   end
 
