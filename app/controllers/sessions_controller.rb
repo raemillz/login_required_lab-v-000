495a3f66f6be9c5accb940e00a4f6c960df9c486
Rachel Miller <raeanne1991@gmail.com>
2018-04-12 21:57:11 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if
-      session[:name] = params[:name]
-      redirect_to 'sessions/show'
-    else
+    if session[:name] = nil
       flash[:alert] = "You must input a name to login."
       redirect_to :new
+
+    else
+      session[:name] = params[:name]
+      redirect_to 'sessions/show'
     end
   end
 
@@ -20,7 +21,7 @@ class SessionsController < ApplicationController
   end
 
   def show
-    
+
   end
 
 end
